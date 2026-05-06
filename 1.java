d15ff17f56ec16eabfe757a4d38f8307227c9700
NAGAVARAPU VISHNU VARDHAN <157562552+vishnuvardhan833199@users.noreply.github.com>
2026-05-06 19:06:47 +0530

    Add sorting check for array based on input conditions
    Implement logic to check if an array is sorted based on input parameters.

--- 1.java
+++ 1.java
@@ -1,1 +1,29 @@
-
+#Halloumi Boxes
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t--> 0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int[] a=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+            }
+            if(k==1){
+                boolean sorted=true;
+                for(int i=1;i<n;i++){
+                    if(a[i]<a[i-1]){
+                        sorted=false;
+                        break;
+                    }
+                }
+                System.out.println(sorted?"YES":"NO");
+            }else{
+                System.out.println("YES");
+            }
+        }
+        sc.close();
+    }
+}
